ed046cb0e9c6e9ddcf84f663fdf76b3def3f2705
Willigue <wiliiamher@yahoo.com>
2021-10-20 16:38:40 -0500

    first commit

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const msg = require('../helpers/messages')
+const User = require('../models/users')
+const authService = require('../services/')
+
+router.post('/registrer', async (req, res) => {
+    try{
+        const user = new User(req.body)
+        const token = await authService.registrer(user)
+        res.status(token.code).json(token);
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+router.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body
+        if(!email || !password){
+            res.status(400).json(msg.fieldsRequired)
+        }
+        const token = await authService.login(req.body)
+        res.status(token.code).json(token)
+    } catch (error) {
+        res.send(error)
+    }
+})
